ae1de28eb8147f27c39cfeeac2ea894d3857522e
Muhammad Ahmad (Temur) Rasul <temur.khan.dispatch@gmail.com>
2025-07-07 18:19:28 -0400

    Update homework.py

--- lesson-1/homework/homework.py
+++ lesson-1/homework/homework.py
@@ -6,3 +6,13 @@ areaOfTheSquere = side** 2
 print('Perimeter is:', perimeterOfTheSquere, '|', 'Areaa is:', areaOfTheSquere)
 
 2.
+diameter = 20
+lengthOfcircle = 3.14 * diameter
+print(lengthOfcircle)
+
+3.
+a = 5
+b = 7
+mean = (a+b) / 2
+
+print(mean)
