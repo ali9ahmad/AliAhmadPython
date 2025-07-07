480a412ecd8c1271dd6f0d3f22daf29d74dbd4dd
Muhammad Ahmad (Temur) Rasul <temur.khan.dispatch@gmail.com>
2025-07-07 18:12:25 -0400

    Update homework.py

--- lesson-1/homework/homework.py
+++ lesson-1/homework/homework.py
@@ -1,1 +1,8 @@
 1.
+side_length = 20
+perimeterOfTheSquere = 4 * side_length
+areaOfTheSquere = side_length ** 2
+
+print('Perimeter is:', perimeterOfTheSquere, '|', 'Areaa is:', areaOfTheSquere)
+
+2.
